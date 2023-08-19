baef8aa178eada5ddecfe0acb97d222797577071
grglaz <grglaza@gmail.com>
2023-08-20 01:06:48 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Αρχική</title>
+	<meta name="description" content="αρχική" />
+</svelte:head>
+
+<section>
+	Αρχική
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Εγγραφή</title>
+	<meta name="description" content="εγγραφή" />
+</svelte:head>
+
+<div class="text-column">
+	<ul>
+		<li>
+			<a href="/register/vet">Εγγραφή ως κτηνιατρείο</a>
+		</li>
+		<li>
+			<a href="/register/org">Εγγραφή ως φιλοζωική</a>
+		</li>
+		<li>
+			<a href="/register/client">Εγγραφή ως χρήστης</a>
+		</li>
+	</ul>
+</div>
